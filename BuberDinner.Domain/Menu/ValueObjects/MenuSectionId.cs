419b28a1ace067ffba2521e02e1611753e315d35
Scott Vaughan <scott.vaughan@ensono.com>
2022-12-13 11:58:54 +0000

    Implement domain objects

---
+++ BuberDinner.Domain/Menu/ValueObjects/MenuSectionId.cs
@@ -0,0 +1,23 @@
+﻿using BuberDinner.Domain.Common.Models;
+
+namespace BuberDinner.Domain.Menu.ValueObjects;
+
+public class MenuSectionId : ValueObject
+{
+    public Guid Value { get; }
+
+    private MenuSectionId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static MenuSectionId CreateUnique()
+    {
+        return new(Guid.NewGuid());
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
